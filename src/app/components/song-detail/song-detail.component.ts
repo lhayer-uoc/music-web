8d21e12a6ae3c2be5de98203f78875338eba030a
Dylanuga <56144502+Dylanuga@users.noreply.github.com>
2022-03-13 18:34:10 +0100

    creación del servicio song y constante SONGS

--- src/app/components/song-detail/song-detail.component.ts
+++ src/app/components/song-detail/song-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Song } from 'src/app/interfaces/song';
 
+
 @Component({
   selector: 'app-song-detail',
   templateUrl: './song-detail.component.html',
