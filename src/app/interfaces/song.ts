fd13cb11cd597606ad162d8bd3098d8b9085d000
hectorbarral <hbarral@uoc.edu>
2022-04-01 19:00:07 +0200

    url

--- src/app/interfaces/song.ts
+++ src/app/interfaces/song.ts
@@ -2,8 +2,11 @@ export interface Song {
   track:number;
   title:string;
   author:string;
+  duration:number;
   year:number;
   country:string;
   album:string;
   genre:string;
+  rating:number;
+  url:string;
 }
