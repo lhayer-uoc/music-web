5ec5e14e599ca145f8f69529cc4e1364a7417444
LHayer-UOC <lhayer@uoc.edu>
2022-04-03 20:51:12 +0200

    player buttons actons

--- src/app/components/player/player.component.ts
+++ src/app/components/player/player.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { Song } from 'src/app/interfaces/song';
 import { SelectedSongService } from 'src/app/services/select-song.service';
+import { SongService } from 'src/app/services/song.service';
 
 @Component({
   selector: 'app-player',
@@ -9,10 +10,13 @@ import { SelectedSongService } from 'src/app/services/select-song.service';
   styleUrls: ['./player.component.css']
 })
 export class PlayerComponent implements OnInit, OnDestroy {
-  constructor(private selectedSongService: SelectedSongService) {}
+  constructor(private selectedSongService: SelectedSongService, private songService: SongService) {}
 
   selectedSong: Song;
   currentSelectedSongSubscription: Subscription;
+  songPath = 'assets/songs/';
+  coverPath = 'assets/images/covers/';
+
 
   ngOnInit(): void {
     this.currentSelectedSongSubscription = this.selectedSongService.currentSelectedSong.subscribe((song: Song) => {
@@ -20,13 +24,54 @@ export class PlayerComponent implements OnInit, OnDestroy {
     });
   }
 
-  getSongCompleteLocalPath(): string {
+  public getSongPath(): string {
     if (this.selectedSong) {
-      const songLocalPath = this.selectedSong.localPath;
-      return `${songLocalPath}`;
+      const songLocalPath = this.selectedSong.url;
+      return `${this.songPath}${songLocalPath}`;
     }
     return '';
+  }
 
+  public getCoverPath(): string {
+    if (this.selectedSong) {
+      const songCoverPath = this.selectedSong.cover_img;
+      return `${this.coverPath}${songCoverPath}`;
+    }
+    return '';
+  }
+
+  public onRandomSong(): void {
+    if (this.selectedSong) {
+      const ramdonSong = this.songService.getRamdonSong();
+      this.selectedSongService.setSelectedSong(ramdonSong);
+    } else {
+      this.playFirstSong();
+    }
+  }
+
+  public onPreviousSong(): void {
+    if (this.selectedSong) {
+      const previousSong = this.songService.getPreviousSong(this.selectedSong);
+      this.selectedSongService.setSelectedSong(previousSong);
+    } else {
+      this.playFirstSong();
+    }
+
+  }
+
+  public onNextSong(): void {
+    if (this.selectedSong) {
+      const nextSong = this.songService.getNextSong(this.selectedSong);
+      this.selectedSongService.setSelectedSong(nextSong);
+    } else {
+      this.playFirstSong();
+    }
+
+  }
+
+  playFirstSong(): void {
+    const firstSong = this.songService.getFirstSong();
+    this.selectedSongService.setSelectedSong(firstSong);
   }
 
   ngOnDestroy(): void {
