a9071e4195c3f43780771f84cfa5630febb67626
Dylanuga <56144502+Dylanuga@users.noreply.github.com>
2022-04-25 23:07:15 +0200

    Componente reproductor material design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,6 @@ import { HeaderComponent } from './components/header/header.component';
 import { MenuComponent } from './components/menu/menu.component';
 import { SongDetailComponent } from './components/song-detail/song-detail.component';
 import { PlayerComponent } from './components/player/player.component';
-<<<<<<< HEAD
 import { MatCardModule } from '@angular/material/card';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -18,13 +17,10 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatListModule } from '@angular/material/list';
 import { MatTableModule } from '@angular/material/table';
 import { MatInputModule } from '@angular/material/input';
-=======
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import { HttpClientModule } from "@angular/common/http";
 import { DomSanitizer } from "@angular/platform-browser";
->>>>>>> Componente-reproductor-Material-Design
 
 @NgModule({
   declarations: [
@@ -40,7 +36,6 @@ import { DomSanitizer } from "@angular/platform-browser";
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-<<<<<<< HEAD
     MatCardModule,
     BrowserAnimationsModule,
     MatFormFieldModule,
@@ -48,13 +43,10 @@ import { DomSanitizer } from "@angular/platform-browser";
     MatListModule,
     BrowserAnimationsModule,
     MatTableModule,
-    MatInputModule
-=======
-    BrowserAnimationsModule,
+    MatInputModule,
     MatButtonModule,
     MatIconModule,
     HttpClientModule,
->>>>>>> Componente-reproductor-Material-Design
   ],
   providers: [],
   bootstrap: [AppComponent]
